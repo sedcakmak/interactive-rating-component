91c05e62778ec022b4800359d6607fedce714e3d
Sedef Cakmak <cakmaksedef@gmail.com>
2022-10-17 19:53:55 +0300

    finalized project

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export default function App() {
       <ratingState.Provider value={{ rating, setRating }}>
         <Routes>
           <Route
-            path="/"
+            path="/interactive-rating-component"
             element={<MainDesign />}
           />
           <Route
